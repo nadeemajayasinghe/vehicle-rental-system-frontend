c391e393a94892b8f1f9de1161fa9cd823d6e8d8
nadeemajayasinghe <nadeemajayasinghe75@gmail.com>
2026-03-10 12:57:55 +0530

    setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-black dark:text-zinc-50">
+          Vehicle Rental System
+        </h1>
+      </main>
+    </div>
+  );
+}
+
