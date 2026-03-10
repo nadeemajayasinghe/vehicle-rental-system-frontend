c06d900449259ebf5d121556ef5fdf8f9ebe70a5
nadeemajayasinghe <nadeemajayasinghe75@gmail.com>
2026-03-10 13:02:57 +0530

    complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,17 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex items-center justify-center">
-        <h1 className="text-4xl font-bold text-black dark:text-zinc-50">
-          Vehicle Rental System
-        </h1>
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-black dark:text-zinc-50 mb-6">
+            15 deadline wada karann ganna moda lamai tika
+          </h1>
+          <ul className="text-2xl text-black dark:text-zinc-50 space-y-2">
+            <li>moda hirun</li>
+            <li>moda vidura</li>
+            <li>moda dilshan</li>
+            <li>hoda nadeema</li>
+          </ul>
+        </div>
       </main>
     </div>
   );
