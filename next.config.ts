b276cd300dc6c4eb41140159018d113af0d01347
hirun athukorala <hirunbhathiya@gmail.com>
2026-03-22 22:00:00 +0530

    sonar test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
+const ALB = "http://api-gateway-alb-1602792189.eu-north-1.elb.amazonaws.com";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/payments/:path*",
+        destination: `${ALB}/payments/:path*`,
+      },
+      {
+        source: "/api/bookings/:path*",
+        destination: `${ALB}/api/bookings/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
